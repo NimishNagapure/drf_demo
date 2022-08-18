1b034db39052a0b557040555f8078cc803c40c41
Nimsh/Cool <nimishnagapure@gmail.com>
2022-08-18 22:28:49 +0530

    Add files via upload

---
+++ auth/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+
+from auth.apis import LoginApi, GoogleLoginApi, LogoutApi
+
+login_patterns = [
+    path('', LoginApi.as_view(), name='login'),
+    path('google/', GoogleLoginApi.as_view(), name='login-with-google'),
+]
+
+urlpatterns = [
+    path('login/', include(login_patterns)),
+    path('logout/', LogoutApi.as_view(), name='logout'),
+    path('', LoginApi.as_view(), name='login'),
+    path('google/', GoogleLoginApi.as_view(), name='login-with-google'),
+]
